c014901e6cf0e3d0b19ef2d2fb9c2c5315dca4d7
MMMaellon <MMMaellon@gmail.com>
2023-06-23 19:46:36 -0700

    added auto-heal and default damage sfx

---
+++ Packages/com.mmmaellon.p-shooters/Runtime/Scripts/AutoHealPlayerListener.cs
@@ -0,0 +1,87 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace MMMaellon.P_Shooters
+{
+    public class AutoHealPlayerListener : PlayerListener
+    {
+        public bool healHealth = false;
+        public bool healShield = true;
+        public float postDamageCooldown = 10f;
+        public int healAmount = 3;
+        public float healInterval = 1f;
+        float lastDamage = -1001f;
+        Player localPlayer;
+        public override void OnDecreaseHealth(Player attacker, Player receiver, int value)
+        {
+            if (!receiver.IsOwnerLocal())
+            {
+                return;
+            }
+            localPlayer = receiver;
+            lastDamage = Time.timeSinceLevelLoad;
+            lastHeal = -1001f;
+        }
+
+        public override void OnDecreaseShield(Player attacker, Player receiver, int value)
+        {
+            if (!receiver.IsOwnerLocal())
+            {
+                return;
+            }
+            localPlayer = receiver;
+            lastDamage = Time.timeSinceLevelLoad;
+            lastHeal = -1001f;
+        }
+        public override void OnMaxHealth(Player healer, Player receiver, int value)
+        {
+            if (!receiver.IsOwnerLocal())
+            {
+                return;
+            }
+            localPlayer = receiver;
+        }
+        public override void OnMaxShield(Player healer, Player receiver, int value)
+        {
+            if (!receiver.IsOwnerLocal())
+            {
+                return;
+            }
+            localPlayer = receiver;
+        }
+
+        float lastHeal = -1001f;
+        public void Update()
+        {
+            if (!Utilities.IsValid(localPlayer))
+            {
+                return;
+            }
+            if (lastHeal + healInterval > Time.timeSinceLevelLoad)
+            {
+                return;
+            }
+            if (lastDamage + postDamageCooldown > Time.timeSinceLevelLoad)
+            {
+                return;
+            }
+            lastHeal = Time.timeSinceLevelLoad;
+            if (healHealth && localPlayer.health < localPlayer.maxHealth)
+            {
+                localPlayer.health = Mathf.Min(localPlayer.maxShield, localPlayer.health + healAmount);
+                if (localPlayer.maxHealth - localPlayer.health < healAmount && healShield && localPlayer.shield < localPlayer.maxShield)
+                {
+                    localPlayer.shield = Mathf.Min(localPlayer.maxShield, localPlayer.shield + (healAmount - localPlayer.maxHealth + localPlayer.health));
+                }
+                return;
+            }
+            if (healShield && localPlayer.shield < localPlayer.maxShield)
+            {
+                localPlayer.shield = Mathf.Min(localPlayer.maxShield, localPlayer.shield + healAmount);
+            }
+        }
+    }
+}
