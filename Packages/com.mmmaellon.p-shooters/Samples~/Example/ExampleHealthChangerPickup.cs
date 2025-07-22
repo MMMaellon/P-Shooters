867d981d1403335de5c382d604c5ec95ddcb1bdc
MMMaellon <mmmaellon@gmail.com>
2025-07-22 02:04:40 -0700

    updated test scene

---
+++ Packages/com.mmmaellon.p-shooters/Samples~/Example/ExampleHealthChangerPickup.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+namespace MMMaellon.P_Shooters
+{
+    public class ExampleHealthChangerPickup : UdonSharpBehaviour
+    {
+        public P_ShootersPlayerHandler playerHandler;
+        public float healthMultiplier = 1.0f;
+        public float shieldMultiplier = 2.0f;
+
+        int prevMaxHealth = 100;
+        int prevMaxShield = 100;
+        public override void OnPickup()
+        {
+            prevMaxHealth = playerHandler.localPlayer.maxHealth;
+            prevMaxShield = playerHandler.localPlayer.maxShield;
+            playerHandler.localPlayer.maxHealth = Mathf.CeilToInt(playerHandler.localPlayer.maxHealth * healthMultiplier);
+            playerHandler.localPlayer.health = Mathf.CeilToInt(playerHandler.localPlayer.health * healthMultiplier);
+            playerHandler.localPlayer.maxShield = Mathf.CeilToInt(playerHandler.localPlayer.maxShield * shieldMultiplier);
+            playerHandler.localPlayer.shield = Mathf.CeilToInt(playerHandler.localPlayer.shield * shieldMultiplier);
+        }
+
+        public override void OnDrop()
+        {
+            playerHandler.localPlayer.maxHealth = prevMaxHealth;
+            playerHandler.localPlayer.health = Mathf.CeilToInt(playerHandler.localPlayer.health * healthMultiplier);
+            playerHandler.localPlayer.maxShield = prevMaxShield;
+            playerHandler.localPlayer.shield = Mathf.CeilToInt(playerHandler.localPlayer.shield * shieldMultiplier);
+        }
+    }
+}
