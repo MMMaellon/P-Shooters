ab4cb5a060c8a353a6d9d6bb9a46ffaec4c64eac
MMMaellon <mmmaellon@gmail.com>
2025-08-22 07:54:13 -0700

    Added safe zone script

---
+++ Packages/com.mmmaellon.p-shooters/Runtime/Scripts/SafeZone.cs
@@ -0,0 +1,40 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace MMMaellon.P_Shooters
+{
+    public class SafeZone : UdonSharpBehaviour
+    {
+        public void OnTriggerEnter(Collider other)
+        {
+            if (!Utilities.IsValid(other))
+            {
+                return;
+            }
+            P_Shooter gun = other.GetComponent<P_Shooter>();
+            if (!Utilities.IsValid(gun) || !gun.sync.IsLocalOwner())
+            {
+                return;
+            }
+            gun.state = P_Shooter.STATE_DISABLED;
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (!Utilities.IsValid(other))
+            {
+                return;
+            }
+            P_Shooter gun = other.GetComponent<P_Shooter>();
+            if (!Utilities.IsValid(gun) || !gun.sync.IsLocalOwner())
+            {
+                return;
+            }
+            gun.state = P_Shooter.STATE_IDLE;
+        }
+
+    }
+}
